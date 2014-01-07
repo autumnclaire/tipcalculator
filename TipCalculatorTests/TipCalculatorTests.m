bf5be8440a6f0d0689040ed6e1ffddbeeec7265e
Autumn Victor <autumnv@stationline-lm.corp.yahoo.com>
2014-01-07 14:26:05 -0800

    Initial Commit

---
+++ TipCalculatorTests/TipCalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  TipCalculatorTests.m
+//  TipCalculatorTests
+//
+//  Created by Autumn Victor on 1/7/14.
+//  Copyright (c) 2014 Autumn Victor. All rights reserved.
+//
+
+#import "TipCalculatorTests.h"
+
+@implementation TipCalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TipCalculatorTests");
+}
+
+@end
